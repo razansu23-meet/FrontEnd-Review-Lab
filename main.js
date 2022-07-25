da4e75a65c6c167b99794a9cc523a371591a0165
JudehAlaaraj <109723372+JudehAlaaraj@users.noreply.github.com>
2022-07-25 18:35:16 +0300

    s

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
-function changeBackgroundColor(color) {
-	document.body.style.background = color;
+var i = 0;
+function changeBackgroundColor(color , color1) {
+	if (i%2==0)
+		{document.body.style.background = color;
+		}
+	else
+	{
+		document.body.style.background = color1;
+		
+	}
+	
+	i++;
+
+
 }
-changeBackgroundColor(pink)
